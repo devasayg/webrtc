16dc645663d762f0bc45ecd2f8100ef1f2d810e5
devasayg <72386275+devasayg@users.noreply.github.com>
2023-04-13 18:24:13 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: '192.168.1.6',
+  host: '/',
   port: '3001'
 })
 const myVideo = document.createElement('video')
